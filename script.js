e7b56ca484013e55c4619b61b1fe6149e19cd2a0
GitDidrik <lindberg.didrik@gmail.com>
2023-04-08 11:27:28 -0700

    error alert if no city entered

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
+var cityFormEl = document.querySelector('#city-form');
+var recentButtonsEl = document.querySelector('#recent-cities-buttons');
+var cityInputEl = document.querySelector('#cityname');
+var currentweatherContainerEl = document.querySelector('#current-weather-container');
+var citySearchTerm = document.querySelector('#city-search-term');
 // const city = document.getElementById('city');
 
 var APIKKey = "4e82548e9a516fc5ec14f1ab6d4a2c48"
 
 
-searchButton.addEventListener('click', () => {
-    var city = searchInput.value;
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKKey;
 
-    fetch(queryURL)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            console.log(queryURL);
-            console.log(data.main.temp);
-            console.log(data.main.humidity);
+// function to get weather data from openweathermap api when user searches for a city
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var cityname = cityInputEl.value.trim();
+
+    if (cityname) {
+        getCityWeather(cityname);
+
+        // clear old content
+        currentweatherContainerEl.textContent = '';
+        cityInputEl.value = '';
+    } else {
+        alert('Please enter a City');
+    }
+};
+
+
+
+// searchButton.addEventListener('click', () => {
+//     var city = searchInput.value;
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKKey;
+
+//     fetch(queryURL)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             console.log(queryURL);
+//             console.log(data.main.temp);
+//             console.log(data.main.humidity);
             // transfer content to html
-            document.querySelector('.city').innerHTML = "City: " + data.name;
-            document.querySelector('.temp').innerHTML = "Temp: " + data.main.temp;
-            document.querySelector('.humidity').innerHTML = "Humidity: " + data.main.humidity;
-            document.querySelector('.wind-speed').innerHTML = "Wind-Speed: " + data.wind.speed;
+        // displayWeather(data);
+            // document.querySelector('.city').innerHTML = "City: " + data.name;
+            // document.querySelector('.temp').innerHTML = "Temp: " + data.main.temp;
+            // document.querySelector('.humidity').innerHTML = "Humidity: " + data.main.humidity;
+            // document.querySelector('.wind-speed').innerHTML = "Wind-Speed: " + data.wind.speed;
             
-            
 
-          
+//         });
+// });
 
-        })
-})
-
-
+cityFormEl.addEventListener('submit', formSubmitHandler);
+recentButtonsEl.addEventListener('click', buttonClickHandler);
